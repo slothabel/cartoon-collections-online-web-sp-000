1cfe51c9518676922cd28a3b1f4487da9040ba8e
Yoshi Mejia <yoshimejia00@gmail.com>
2021-05-14 20:33:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,8 @@ calls.each do |i|
  return false
   end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
